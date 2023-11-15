27c923153552fc23ed9ae7680079baba5a9e0646
bitxuji <bitxuji@gmail.com>
2023-11-15 21:26:17 +0900

    add some rules to eslint

--- src/app/services/github.followers.service.ts
+++ src/app/services/github.followers.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
-import { DataService } from "./data.service";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class GithubFollowersService extends DataService {
